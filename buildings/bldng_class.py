5e67bae2279adeaaaf2bebda2debd1b5c906b47e
Andrew-Kelley <akelley2500@gmail.com>
2018-07-25 16:25:29 -0600

    Fill in TownCenter class and add __init__
    Add __init__ method to Building class

--- buildings/bldng_class.py
+++ buildings/bldng_class.py
@@ -1,5 +1,13 @@
 # To see which files contain which buildings, see directory_structure.txt
 
 class Building:
-    pass
+    # Have a method to handle when a building is being attacked. Also have a method to handle building
+    # destruction.
+    # Perhaps have a method to handle
+    # the buildings which are defensible (i.e. which shoot arrows at attackers if there are units garrisoned
+    # in it.
+    def __init__(self, number):
+        """For each player, the first of each building is numbered 1.
+        Further buildings built (of the same type) are consecutively numbered."""
+        self.number = number
 
--- buildings/other_bldngs.py
+++ buildings/other_bldngs.py
@@ -1,7 +1,16 @@
 from buildings.bldng_class import Building
 
 class TownCenter(Building):
-    pass
+    """Every player begins with one TownCenter.
+    More can be built in the Iron Age."""
+    cost = {'wood':300, 'stone':200}
+    size = (4,4)
+    # hit_points = ?
+    # construction_time = ?
+    population_support = 20
+    garrison_capacity = 50
+    defensible = True
+
 
 class House(Building):
     pass
@@ -12,7 +21,11 @@ class Blacksmith(Building):
 class Library(Building):
     pass
 
-
 # Maybe implement this building last
 class Market(Building):
     pass
+
+if __name__ == '__main__':
+    t = TownCenter(1)
+    print(t.cost)
+    print(t.number)
