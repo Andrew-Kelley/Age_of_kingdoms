7c87fc486660d46b4c2d1cf18eecf9c28b0831d7
Andrew-Kelley <akelley2500@gmail.com>
2018-07-26 18:38:12 -0600

    Move game_map and Position class to its own file

---
+++ game_map.py
@@ -0,0 +1,28 @@
+class Position:
+    """The i and j coordinates of a position on the map"""
+    def __init__(self, i, j):
+        self.value = (i, j)
+
+    def __add__(self, other):
+        i = self.value[0] + other.value[0]
+        j = self.value[1] + other.value[1]
+        return Position(i, j)
+
+    def __repr__(self):
+        return str(self.value)
+
+# Eventually, I should create a function that makes a random map
+game_map  = [[' '] * 100 for i in range(100)]
+for i in range(60, 66):
+    for j in range(75, 85):
+        game_map[i][j] = 'w'
+
+# Eh, I feel like adding more wood:
+for i in range(66, 72):
+    for j in range(80, 90):
+        game_map[i][j] = 'w'
+
+# For larger maps, this function should really only print part of the map (which would need to be specified).
+def print_map(some_map):
+    for ls in game_map:
+        print(''.join(ls))
