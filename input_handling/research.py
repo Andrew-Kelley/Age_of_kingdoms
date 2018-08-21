b15bd93e7c0ea117037502bbd4f29258d46b93e3
Andrew-Kelley <akelley2500@gmail.com>
2018-08-20 20:08:26 -0600

    Change what the fn research_something returns
    Specifically, make thing_to_be_researched to be a subclass of
    ResearchObject.

--- input_handling/research.py
+++ input_handling/research.py
@@ -1,5 +1,6 @@
 from input_handling.select_an_object import selected_obj_to_actual_building
 from buildings.bldng_class import Building
+from research_classes import research_string_to_class
 
 
 def research_something(player, inpt_as_ls, selected_obj=None, selected_town_num=1):
@@ -8,7 +9,8 @@ def research_something(player, inpt_as_ls, selected_obj=None, selected_town_num=
     'word1 word2' must be something that can be researched from the selected building.
 
     Returns [] or
-    ['research', building, thing_to_be_researched]"""
+    ['research', building, thing_to_be_researched], where thing_to_be_researched is a
+    subclass of ResearchObject."""
     if not selected_obj:
         print('You must first select a building to research something.')
         return []
@@ -23,6 +25,15 @@ def research_something(player, inpt_as_ls, selected_obj=None, selected_town_num=
     if thing_to_be_researched not in building.strings_ls_of_things_which_can_be_researched(player):
         print('Sorry, a {} cannot research {}.'.format(building.kind, thing_to_be_researched))
         return []
+
+    if thing_to_be_researched not in research_string_to_class:
+        print('Error. This game is still unfinished, and researching',
+              thing_to_be_researched, 'has not yet been enabled.',
+              'Developer note: specifically, ', thing_to_be_researched,
+              'has not yet been added to the research_string_to_class dictionary.')
+        return []
+
+    thing_to_be_researched = research_string_to_class[thing_to_be_researched]
 
     return ['research', building, thing_to_be_researched]
 
