1197bd3d6c6e28e4ff5805f2acfd2acb75d8a081
Andrew-Kelley <akelley2500@gmail.com>
2020-08-21 13:59:13 -0600

    Fix docstring

--- input_handling/move_units.py
+++ input_handling/move_units.py
@@ -1,26 +1,23 @@
 from input_handling.direction_vector import get_direction_vector
 from input_handling.select_an_object import SelectedUnits, extract_selected_obj
 
+
 def move_unit_or_units(player, inpt_as_ls, selected_obj=None):
-    """If selected_obj must be None or an instance of SelectedObject
+    """Returns [] or ['move', ls_of_units, delta],
+    where delta is of type Vector
 
-    In order for this function to not return [], inpt_as_ls must be of the following
-    type:
-    (In what follows, each list may end with one or two entries of <direction string>)
+    selected_obj must be None or an instance of SelectedUnits
 
-    # WHAT DID I MEAN BY THE FOLLOWING LINE?
-    # In the first two cases, selected_obj must be of the type that select_something
-    returns ['move', <direction string>], or
-
+    In order for this function to not return [], inpt_as_ls must be of the
+    following type: (In what follows, <direction string> can be one or two
+    entries of the list.)
+    ['move', <direction string>] where selected_obj is not None, or
     ['move', unit.kind singular, 'unit.number', <direction string>], or
     ['move', unit.kind plural, 'num1-num2', <direction string>], or
     ['move', 'group', 'group_num', <direction string>], or
-    ['move', 'army', 'army_num', <direction string>], where
-    <direction string> must be formatted such that the fn direction_inpt_to_vector
-    does not return None.
-
-    Returns: [] or
-    ['move', ls_of_units, delta], where delta is of type Vector
+    ['move', 'army', 'army_num', <direction string>].
+    In each case, <direction string> must be formatted such that the
+    fn direction_inpt_to_vector does not return None.
     """
     if len(inpt_as_ls) < 2:
         return []
