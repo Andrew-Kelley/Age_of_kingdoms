e1362980afba27a8f6e4bff30f6bcc21b1f68d43
Andrew-Kelley <akelley2500@gmail.com>
2020-09-08 21:20:42 -0600

    Fill in class class FarmCmd +more
    more:
    Remove name mangling based on the recommendation here:
    https://stackoverflow.com/questions/7456807/python-name-mangling
    
    Also, in the the BuildBuildingCmd class:
     - don't turn the villagers iterator into a list.
     - initialize villagers to an empty tuple in case the the
     __init__ method fails to properly execute.

--- commands.py
+++ commands.py
@@ -2,9 +2,7 @@
 # are just lists. It would be better if they were instead their
 # own classes. Then instead of accessing a part of a command through
 # a quite arbitrary list index, you'd instead use an (informative)
-# attribute name. Also, if an instance is created, then that should
-# guarantee that the command is proper (with all its attributes
-# initialized).
+# attribute name.
 
 # Doing this change would probably be helpful for when I add more
 # commands such as follows:
@@ -23,6 +21,7 @@
 
 from units import Unit, unit_kinds, Villager
 from buildings.bldng_class import Building
+from buildings.resource_bldngs import Farm
 from game_map import Vector, Position
 from research_classes import ResearchObject
 from resources import Resource
@@ -31,13 +30,17 @@ from inspect import isclass
 
 
 class Command:
-    # The following should always be overridden by subclasses
-    kind = 'a command'
-    __is_initialized = False
+    # The _kind attribute should always be overridden by subclasses
+    _kind = 'a command'
+    _is_initialized = False
 
     @property
     def is_initialized(self):
-        return self.__is_initialized
+        return self._is_initialized
+
+    @property
+    def kind(self):
+        return self._kind
 
 
 class MoveCmd(Command):
@@ -46,32 +49,33 @@ class MoveCmd(Command):
     # to a given position (instead of saying all should move by te same
     # delta). Hence each unit should have its own delta.
 
-    kind = 'move'
+    _kind = 'move'
 
     def __init__(self):
-        self.__unit_delta_pairs = []
+        self._unit_delta_pairs = []
         # As this is guaranteed to run when a MoveCmd instance is created,
-        # I will instead set self.__is_initialized to True if at least
+        # I will instead set self._is_initialized to True if at least
         # one (unit, delta) pair has been successfully added.
 
     def add_unit_with_delta(self, unit, delta):
+        message = "Error. Developer message: MoveCmd.__init__ was called,"
         if not isinstance(unit, Unit):
-            print("Error. Developer message: MoveCmd.__init__ was called,")
+            print(message)
             print("but unit was not an instance of Unit.")
             return
         if not isinstance(delta, Vector):
-            print("Error. Developer message: MoveCmd.__init__ was called,")
+            print(message)
             print("but delta was not an instance of Vector.")
             return
-        self.__unit_delta_pairs.append((unit, delta))
-        self.__is_initialized = True
+        self._unit_delta_pairs.append((unit, delta))
+        self._is_initialized = True
 
     def units(self):
-        for pair in self.__unit_delta_pairs:
+        for pair in self._unit_delta_pairs:
             yield pair[0]
 
     def units_and_deltas(self):
-        for pair in self.__unit_delta_pairs:
+        for pair in self._unit_delta_pairs:
             yield pair
 
 
@@ -80,47 +84,46 @@ class BuildUnitCmd(Command):
     # building was an instance of Building
     # unit_type was a string in unit_kinds from units.py
 
-    kind = 'build unit'
-    __building = None
-    __unit_kind = None
-    __num_to_build = None
+    _kind = 'build unit'
+    _building = None
+    _unit_kind = None
+    _num_to_build = None
 
     def __init__(self, building, unit_kind, num_to_be_built):
+        message = "Error. Developer message: BuildUnitCmd.__init__ was called,"
         if not isinstance(building, Building):
-            print("Error. Developer message: BuildUnitCmd.__init__ was called,")
+            print(message)
             print("but building was not an instance of Building.")
             return
         if unit_kind not in unit_kinds:
-            print("Error. Developer message: BuildUnitCmd.__init__ was called,")
+            print(message)
             print("but unit_kind was not in unit_kinds.")
             return
         if not type(num_to_be_built) is int:
-            print("Error. Developer message: BuildUnitCmd.__init__ was called,")
+            print(message)
             print("but num_to_be_built was not an int.")
             return
         if num_to_be_built < 1:
-            print("Error. Developer message: BuildUnitCmd.__init__ was called,")
+            print(message)
             print("but num_to_be_built < 1")
             return
         # if unit_kind not in building.units_which_can_be_built()
-        self.__building = building
-        self.__unit_kind = unit_kind
-        self.__num_to_build = num_to_be_built
-        self.__is_initialized = True
+        self._building = building
+        self._unit_kind = unit_kind
+        self._num_to_build = num_to_be_built
+        self._is_initialized = True
 
     @property
     def building(self):
-        return self.__building
+        return self._building
 
     @property
     def unit_kind(self):
-        return self.__unit_kind
+        return self._unit_kind
 
     @property
     def num_to_build(self):
-        return self.__num_to_build
-
-
+        return self._num_to_build
 
 
 class BuildBuildingCmd(Command):
@@ -129,12 +132,14 @@ class BuildBuildingCmd(Command):
     # position was an instance of Position
     # this_is_a_help_build_command was a bool
 
-    kind = 'build building'
-    __building_class = None
-    __position = None
-    __is_a_help_build_cmd = None
+    _kind = 'build building'
+    _villagers = tuple()
+    _building_class = None
+    _position = None
+    _is_a_help_build_cmd = None
 
     def __init__(self, villagers, building_class, position, is_a_help_build_cmd):
+        # villagers can be any iterator of villagers
         message = "Error. Developer message: BuildBuildingCmd.__init__ was called,"
         for v in villagers:
             if not isinstance(v, Villager):
@@ -153,30 +158,36 @@ class BuildBuildingCmd(Command):
             print(message)
             print("and position was not an instance of Position.")
             return
-        def is_bool(obj):
-            return obj is False or obj is True
+        
+        def is_bool(x):
+            return x is False or x is True
+        
         if not is_bool(is_a_help_build_cmd):
             print(message)
             print("and is_a_help_build_cmd was not a bool.")
             return
 
-        self.__building_class = building_class
-        self.__position = position
-        self.__is_a_help_build_cmd = is_a_help_build_cmd
-        self.__is_initialized = True
+        self._villagers = villagers
+        self._building_class = building_class
+        self._position = position
+        self._is_a_help_build_cmd = is_a_help_build_cmd
+        self._is_initialized = True
+
+    def villagers(self):
+        for villager in self._villagers:
+            yield villager
 
     @property
     def building_class(self):
-        return self.__building_class
+        return self._building_class
 
     @property
     def position(self):
-        return self.__position
+        return self._position
 
     @property
     def is_a_help_build_cmd(self):
-        return self.__is_a_help_build_cmd
-
+        return self._is_a_help_build_cmd
 
 
 class ResearchCmd(Command):
@@ -184,9 +195,9 @@ class ResearchCmd(Command):
     # building was an instance of Building
     # thing_to_be_researched was an instance of a subclass of ResearchObject
 
-    kind = 'research'
-    __building = None
-    __thing_to_be_researched = None
+    _kind = 'research'
+    _building = None
+    _thing_to_be_researched = None
 
     def __init__(self, building, thing_to_be_researched):
         message = "Error. Developer message: ResearchCmd.__init__ was called,"
@@ -199,26 +210,26 @@ class ResearchCmd(Command):
             print("but thing_to_be_researched was not an instance of")
             print("ResearchObject.")
             return
-        self.__building = building
-        self.__thing_to_be_researched = thing_to_be_researched
-        self.__is_initialized = True
+        self._building = building
+        self._thing_to_be_researched = thing_to_be_researched
+        self._is_initialized = True
 
     @property
     def building(self):
-        return self.__building
+        return self._building
 
     @property
     def thing_to_be_researched(self):
-        return self.__thing_to_be_researched
+        return self._thing_to_be_researched
 
 
 class CollectResourceCmd(Command):
     # Prior format: ['collect resource', resource, ls_of_villagers]
     # resource was one of the classes Food, Gold, Stone, etc.
 
-    kind = 'collect resource'
-    __resource = None
-    __villagers = tuple()
+    _kind = 'collect resource'
+    _resource = None
+    _villagers = tuple()
 
     def __init__(self, resource, villagers):
         message = "Error. Developer message: CollectResourceCmd.__init__ was called,"
@@ -232,18 +243,16 @@ class CollectResourceCmd(Command):
             print("and resource was not an instance of Resource.")
             return
 
-        self.__resource = resource
-        self.__villagers = []
-        for villager in villagers:
-            self.__villagers.append(villager)
-        self.__is_initialized = True
+        self._resource = resource
+        self._villagers = villagers
+        self._is_initialized = True
 
     @property
     def resource(self):
-        return self.__resource
+        return self._resource
 
     def villagers(self):
-        for villager in self.__villagers:
+        for villager in self._villagers:
             yield villager
 
 
@@ -251,15 +260,40 @@ class FarmCmd(Command):
     # Prior format: ['farm', farm, ls_of_villagers]
     # farm was an instance of Farm
 
-    kind = 'farm'
+    _kind = 'farm'
+    _farm = None
+    _villagers = None
+
+    def __init__(self, farm, villagers):
+        message = "Error. Developer message: FarmCmd.__init__ was called,"
+        if not isinstance(farm, Farm):
+            print(message)
+            print("and farm was not an instance of Farm.")
+            return
+        for v in villagers:
+            if not isinstance(v, Villager):
+                print(message)
+                print("and some element of villagers was not a villager.")
+                return
+        self._farm = farm
+        self._villagers = villagers
+        self._is_initialized = True
+
+    def villagers(self):
+        for villager in self._villagers:
+            yield villager
+
+    @property
+    def farm(self):
+        return self._farm
 
 
 class EndOfTurnCmd(Command):
     # Prior format: ['end of turn']
-
-    kind = 'end of turn'
+    _kind = 'end of turn'
 
 
 if __name__ == '__main__':
     obj = BuildUnitCmd(1, 2, 3)
     print(obj.building)
+    print(obj.kind)
