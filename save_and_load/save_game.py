9cdb18578c815212b76cfc38eb660b772aacee15
Andrew-Kelley <akelley2500@gmail.com>
2020-09-10 15:12:52 -0600

    Change fn get_name_of_file
    It now produces the name of the file automatically, based
    solely on the current date and time.
    
    Also, add a print statement to fn save_game so it tells the user
    when it successfully saved a game.

--- save_and_load/save_game.py
+++ save_and_load/save_game.py
@@ -2,23 +2,49 @@
 # commands that each player enters. (And as of now, all players
 # are human.)
 
+# TODO: These also need to be edited:
+# - the set_default_build_position fn in input_handling/build.py
+# - the research_something fn in input_handling/research.py
+
+from datetime import datetime
+
+
+def replace_with(string, index, replacement):
+    """replace character at index with replacement string"""
+    return string[:index] + replacement + string[index+1:]
+
 
 def get_name_of_file():
-    while True:
-        inpt = input("Enter the name of the file you'd like to save the game as: ")
-        if inpt == '':
-            continue
-        if len(inpt) < 4:
-            print("Your file name is too short.")
-            continue
-        if inpt[-4:] != '.txt':
-            print("Your file name must end in .txt")
-            continue
-        if ' ' in inpt:
-            print("You cannot contain spaces in the file name.")
-            continue
+    """The name of the file is based solely on the current date and time."""
+    date = str(datetime.today())
 
-        return inpt
+    colon_index = date.index(':')
+    date = replace_with(date, colon_index, 'hr')
+
+    colon_index = date.index(':')
+    date = replace_with(date, colon_index, 'min')
+
+    date = date.replace(' ', '_')
+    period_index = date.index('.')
+    return "saved_at_" + date[:period_index] + "sec.txt"
+    # When I first wrote this function, I had it ask the user for the
+    # name of the file.
+    # while True:
+    #     inpt = input("Enter the name of the file you'd like to save the game as: ")
+    #     if inpt == '':
+    #         continue
+    #     if len(inpt) < 4:
+    #         print("Your file name is too short.")
+    #         continue
+    #     if inpt[-4:] != '.txt':
+    #         print("Your file name must end in .txt")
+    #         continue
+    #     if ' ' in inpt:
+    #         print("You cannot contain spaces in the file name.")
+    #         continue
+    #
+    #     return inpt
+
 
 def still_saving_given(indexes, players):
     """Checks value of indexes to see if saving is still in progress."""
@@ -57,5 +83,5 @@ def save_game(players):
                         break
 
             if not still_saving_given(indexes, players):
+                print('Game successfully saved.')
                 return
-
