f50915431a016ef64efa5a13399ac20324c20300
Andrew-Kelley <akelley2500@gmail.com>
2020-11-18 14:59:33 -0700

    Add command: 'move to <absolute position>'
    Here, <absolute position> is just a space separated pair of
    integers: i j
    This represents Position(i, j) on the map.

--- input_handling/move_units.py
+++ input_handling/move_units.py
@@ -1,6 +1,8 @@
 from input_handling.direction_vector import get_direction_vector
 from input_handling.select_an_object import SelectedUnits, extract_selected_obj
+from input_handling.from_ls_get_position import get_position_from_inpt_as_ls
 from command_handling.commands import MoveCmd
+
 
 def move_unit_or_units(player, inpt_as_ls, selected_obj=None):
     """Returns None or an instance of MoveCmd
@@ -10,6 +12,10 @@ def move_unit_or_units(player, inpt_as_ls, selected_obj=None):
     In order for this function to not return None, inpt_as_ls must be of the
     following type: (In what follows, <direction string> can be one or two
     entries of the list.)
+    ['move', 'to', i, j] where selected_obj is not None and (i, j) is a
+                         position on the map.
+                         (Actually ['move', 'to', ..., i, j] works where what
+                         is in the ellipsis is ignored.
     ['move', <direction string>] where selected_obj is not None, or
     ['move', unit.kind singular, 'unit.number', <direction string>], or
     ['move', unit.kind plural, 'num1-num2', <direction string>], or
@@ -21,34 +27,55 @@ def move_unit_or_units(player, inpt_as_ls, selected_obj=None):
     if len(inpt_as_ls) < 2:
         return
 
+    if inpt_as_ls[1] == 'to':
+        # This is a 'move to absolute position' command
+        position_to_move_to = get_position_from_inpt_as_ls(inpt_as_ls)
+        if not position_to_move_to:
+            print("Command rejected. The position to move your unit(s) to in your")
+            print("command was not understood.")
+            return
+
+        if nothing_is_selected(selected_obj):
+            return
+
+        command = MoveCmd()
+        for unit in selected_obj.units:
+            delta = position_to_move_to - unit.position
+            command.add_unit_with_delta(unit, delta)
+        return command
+
+    # If this is reached, then the command is to move units to a position relative
+    # to the units' positions (such as n10 e4 i.e. north 10 and east 4)
     delta = get_direction_vector(inpt_as_ls)
     if delta is None:
         return
 
     if len(inpt_as_ls) in {2, 3}:
-        # Then the player is trying to move selected_obj
+        # Then the player is trying to move selected_obj and
 
-        if not isinstance(selected_obj, SelectedUnits):
-            # TODO: edit this once I add the Army and Group class?
-            return
-        if selected_obj.is_empty:
+        if nothing_is_selected(selected_obj):
             return
 
         command = MoveCmd()
         for unit in selected_obj.units:
             command.add_unit_with_delta(unit, delta)
         return command
-
     else:  # len(inpt_as_ls) > 3
         selected_obj = extract_selected_obj(inpt_as_ls, player)
-        if not isinstance(selected_obj, SelectedUnits):
-            # TODO: edit this once I add the Army and Group class?
-            return
-
-        if selected_obj.is_empty:
+        if nothing_is_selected(selected_obj):
             return
 
         command = MoveCmd()
         for unit in selected_obj.units:
             command.add_unit_with_delta(unit, delta)
         return command
+
+
+def nothing_is_selected(selected_obj):
+    if not isinstance(selected_obj, SelectedUnits):
+        # TODO: edit this once I add the Army and Group class?
+        return True
+    if selected_obj.is_empty:
+        return True
+
+    return False
