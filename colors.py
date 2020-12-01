66882e55a4a66bb3a2838b9c3024cb5e7ce5ea54
Andrew-Kelley <akelley2500@gmail.com>
2020-12-01 14:25:03 -0700

    Add background gray color

--- colors.py
+++ colors.py
@@ -18,13 +18,33 @@ class Color:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+    BACK_GRAY = '\033[47m'
+
     ENDC = '\033[0m'
+
+    # This was only used to print all colors to find what would be a good background
+    # color.
+    @classmethod
+    def return_color(self, num):
+        return '\033[' + str(num) + 'm'
 
 
 
 if __name__ == '__main__':
-    print(Color.LIGHT_BLUE + "light blue" + Color.ENDC)
-    print(Color.DARK_BLUE + "blue" + Color.ENDC)
+    print(Color.DARK_BLUE + "dark blue" + Color.ENDC)
+    print(Color.GREEN + "green" + Color.ENDC)
+    print(Color.MAGENTA + "magenta" + Color.ENDC)
     print(Color.UNDERLINE + Color.RED + "R" + Color.ENDC)
-    print(Color.BOLD + Color.DARK_BLUE + "blue" + Color.ENDC)
+    print(Color.LIGHT_BLUE + "light blue" + Color.ENDC)
+
+    print(Color.BOLD + Color.DARK_BLUE + "dark blue bold" + Color.ENDC)
+    print(Color.BOLD + Color.LIGHT_BLUE + "light blue bold" + Color.ENDC)
     print("Is color back to normal now?")
+
+    print(Color.BACK_GRAY + "Hello people" + Color.ENDC)
+
+    for color in (Color.DARK_BLUE, Color.GREEN, Color.MAGENTA, Color.RED, Color.LIGHT_BLUE):
+        print(Color.BACK_GRAY + color + "some text" + Color.ENDC)
+
+    # for n in range(1, 256):
+    #     print(n, Color.return_color(n) + "some text" + Color.ENDC)
