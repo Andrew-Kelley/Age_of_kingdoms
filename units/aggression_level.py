c4a6c50aa737a14f6f27da8f4cb24d8eebe127a6
Andrew-Kelley <akelley2500@gmail.com>
2020-12-29 16:20:50 -0700

    Add file to name constants

---
+++ units/aggression_level.py
@@ -0,0 +1,6 @@
+# These levels are to be used to determine if a unit attacks or flees when it
+# is attacked etc.
+
+FLEE = 0        # FLEE means to flee when attacked.
+DEFEND = 1      # DEFEND means to defend if attacked, but don't pursue.
+AGGRESSIVE = 2  # AGGRESSIVE means to pursue enemies until commanded otherwise.
