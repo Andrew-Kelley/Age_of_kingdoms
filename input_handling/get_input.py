b1a9e7c8b0794f3ca12a8ca881ea4a3dfefdca3f
Andrew-Kelley <akelley2500@gmail.com>
2021-01-25 10:07:17 -0700

    Rename a variable

--- input_handling/get_input.py
+++ input_handling/get_input.py
@@ -13,21 +13,21 @@ from command_handling.commands import SaveGameCmd, EndOfTurnCmd
 def input_number_of_players(human=True):
     if human:
         min_number = 1
-        text = 'human'
+        player_type = 'human'
         max_number = 2
     else:
         min_number = 0
-        text = 'computer'
+        player_type = 'computer'
         max_number = 7
     while True:
-        num_players = input('How many {} players are there? '.format(text))
+        num_players = input('How many {} players are there? '.format(player_type))
         try:
             num_players = int(num_players)
             if min_number <= num_players <= max_number:
                 return num_players
             else:
                 print("""Only {0} to {1} {2} players can play at the same time. 
-                Enter a number from {0} to {1}.""".format(min_number, max_number, text))
+                Enter a number from {0} to {1}.""".format(min_number, max_number, player_type))
         except ValueError:
             print('Please enter a numeral (such as 1 or 2).')
 
